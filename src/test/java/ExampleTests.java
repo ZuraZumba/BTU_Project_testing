87ced0cfc59856829f5160235a163dd6288763f1
ATkabladze <alinatkabladze@gmail.com>
2023-03-29 20:51:45 +0400

    Lecture ||

--- src/test/java/ExampleTests.java
+++ src/test/java/ExampleTests.java
@@ -3,17 +3,29 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
 public class ExampleTests {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+    }
+
 
     @Test
     public void firstTest(){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
         driver.get("http://the-internet.herokuapp.com/");
         driver.manage().window().maximize();
         WebElement button=driver.findElement(By.xpath("//ul/li/a"));
@@ -23,16 +35,34 @@ public class ExampleTests {
     }
     @Test
     public void locatorTest(){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
         driver.get("http://tutorialsninja.com/demo/index.php?route=product/category&path=33");
         driver.manage().window().maximize();
         //*[@id="content"]/div/div[2]/h1 - XPath
         //#content > div > div.col-sm-4 > h1 - css
         List<WebElement> anchors = driver.findElements(By.className("product-thumb"));
-        Assert.assertEquals(anchors.size(),2);
+        Assert.assertEquals(anchors.size(),4,"Check count");
          WebElement button=driver.findElement(By.partialLinkText("Canon EOS 5D"));
         button.click();
+    }
+    @Test
+    public void dropdownTest(){
+        driver.get("http://the-internet.herokuapp.com/dropdown");
+        driver.manage().window().maximize();
+        WebElement element=driver.findElement(By.id("dropdown"));
+        Select select = new Select(element);
+        select.selectByIndex(1);
+    }
+    @Test
+    public void checkBoxTest(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+         driver = new ChromeDriver(options);
+        driver.get("http://the-internet.herokuapp.com/checkboxes");
+        driver.manage().window().maximize();
+        WebElement element=driver.findElement(By.xpath("//form[@id='checkboxes']/input[2]"));
+        element.click();
+
     }
     @Test
     public void browserCommands(){
@@ -48,5 +78,9 @@ public class ExampleTests {
         // Get the current page HTML source
         String html = driver.getPageSource();
         System.out.println("HTML " + html);
+    }
+    @AfterMethod
+    public void closeBrowser(){
+        driver.quit();
     }
 }
--- src/test/java/Waits.java
+++ src/test/java/Waits.java
@@ -3,8 +3,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -16,7 +18,10 @@ public class Waits {
 
     public Waits() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(2, SECONDS);
     }
     @Test
     public void waitExplicitly() {
@@ -24,13 +29,15 @@ public class Waits {
         driver.manage().window().maximize();
         WebElement submitButton = driver.findElement(By.xpath("//form[@id='input-example']/button[@type='button']"));
         submitButton.click();
-        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"input-example\"]/input")));
+        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"input-example\"]/input")));
+        WebElement text=driver.findElement(By.id("message"));
+        Assert.assertEquals(text.getText(),"It's enabled!");
     }
     @Test
     public void waitImplicitly() {
         driver.get("http://the-internet.herokuapp.com/dynamic_controls");
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(5, SECONDS);
+
         WebElement submitButton = driver.findElement(By.xpath("//form[@id='input-example']/button[@type='button']"));
         submitButton.click();
         WebElement text = driver.findElement(By.xpath("//*[@id=\"message\"]"));
